6a6ee3fa8c3352409d3d036d2e49d2d4bbae39ea
Yuvraj3079 <yuvrajsingh.ys736@gmail.com>
2023-08-09 23:39:59 -0700

    Lesson2 done. Added logo and navigation bar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import Home from "./routes/home/home.component";
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>This is Navigation</h1>
-      </div>
-      <Outlet/>
-    </div>
-  )
-}
-
+import Navigation from "./routes/navigation/navigation.component";
 const Shop = () =>{
   return (
     <div>
