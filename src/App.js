fdb70d9f1b3e45d4a6c6f846e51186ac1717f70a
Yuvraj3079 <yuvrajsingh.ys736@gmail.com>
2023-08-09 22:23:06 -0700

    Added First Route

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
-import Directory from "./components/directory/directory.component";
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-  console.log(1);
   return (
-    <Directory categories={categories} />
-    // <div className="categories-container">
-    //   {categories.map((category) => (
-    //     <CategoryItem key={category.id} category={category} />
-    //   ))}
-    // </div>
-  );
+    <Routes>
+      <Route path="/" element={<Home />} />
+      
+    </Routes>
+  );  
 };
 
 export default App;
