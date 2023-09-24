726cd1831c4582a2be59e936f7ea92ecb75c4fac
Yuvraj3079 <yuvrajsingh.ys736@gmail.com>
2023-09-24 10:54:59 -0700

    added product categories to Firestore

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,27 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-import PRODUCTS from "../shop-data.json";
+import { getCategoriesAndDcouments } from "../utils/firebase/firebase.util";
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+  /*
+to get the data in FireStore
+  useEffect(() => {
+    addCollectionAndDcouments("categories", SHOP_DATA);
+  }, []);
+*/
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const catgoryMap = await getCategoriesAndDcouments();
+      console.log(catgoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+
   const value = { products };
   return (
     <ProductsContext.Provider value={value}>
